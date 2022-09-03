944547b88efc029eb3a07e4709682a478fb7ed8b
Allan Uustal <allan.uustal@gmail.com>
2022-09-03 10:30:47 +0300

    [change] -Hotel Facilities Converter in Java 7 style

--- backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
+++ backend/src/main/java/com/sda/travelagency/converter/HotelFacilitiesConverter.java
@@ -1,15 +1,33 @@
 package com.sda.travelagency.converter;
 
+import com.sda.travelagency.dto.AttractionDto;
 import com.sda.travelagency.dto.HotelFacilitiesDto;
+import com.sda.travelagency.entity.Attraction;
 import com.sda.travelagency.entity.HotelFacilities;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
 public class HotelFacilitiesConverter implements Converter<HotelFacilities, HotelFacilitiesDto>{
+    private final AttractionConverter attractionConverter;
+
+    public HotelFacilitiesConverter(AttractionConverter attractionConverter) {
+        this.attractionConverter = attractionConverter;
+    }
+
     @Override
     public HotelFacilitiesDto fromEntityToDto(HotelFacilities entity) {
-        return null;
+        //till Java 8
+        List<AttractionDto> attractionsDtos = new ArrayList<>();
+        for (Attraction attraction: entity.getAttractions()){
+            AttractionDto attractionDto = attractionConverter.fromEntityToDto(attraction);
+            attractionsDtos.add(attractionDto);
+        }
+
+        return new HotelFacilitiesDto(null,entity.getApartmentFacilities());
     }
 
     @Override

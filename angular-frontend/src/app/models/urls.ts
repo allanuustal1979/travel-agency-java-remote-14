4444d51f04f74182798dc8f4e82990cb18527675
Allan Uustal <allan.uustal@gmail.com>
2022-09-04 10:14:46 +0300

    [refactor] - better way of preparing urls for server connection

--- angular-frontend/src/app/models/urls.ts
+++ angular-frontend/src/app/models/urls.ts
@@ -1,2 +1,4 @@
 //
-const allTrips = 'http://localhost:8080/api/trips'
+const serverUrl = 'http://localhost:8080'
+export const allTripsUrl = `${serverUrl}/api/trips`
+// const allTripsUrlJavaWay = serverUrl + '/api/trips' //making it with Java way

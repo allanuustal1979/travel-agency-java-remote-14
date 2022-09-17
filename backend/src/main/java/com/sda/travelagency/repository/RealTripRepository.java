eaff12b405a653d2ea79bf99baf0e266c441ead8
allan.uustal@gmail.com <$t@ULZ3vyYtD,L8>
2022-09-17 13:21:08 +0300

    [change] - using two profiles (develop and default) in order to make our tests easier

--- backend/src/main/java/com/sda/travelagency/repository/RealTripRepository.java
+++ backend/src/main/java/com/sda/travelagency/repository/RealTripRepository.java
@@ -1,7 +1,13 @@
 package com.sda.travelagency.repository;
 
 import com.sda.travelagency.entity.Trip;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Base64;
+
+@Profile("!Develop"
+)
 
 public interface RealTripRepository extends JpaRepository<Trip, Long> {
 }
